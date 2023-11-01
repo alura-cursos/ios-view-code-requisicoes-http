2b05f95a7433cc99c409291921a8b4d48141d824
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-11-01 17:14:00 -0300

    aula 3.2 - conhecendo o completion handler

--- Cinetopia/Services/MovieService.swift
+++ Cinetopia/Services/MovieService.swift
@@ -8,30 +8,30 @@
 import Foundation
 
 struct MovieService {
-    func getMovies() -> [Movie]? {
+    func getMovies(completion: @escaping ([Movie]?) -> Void) {
         
         var movies: [Movie] = []
         
         let urlString = "http://localhost:3000/movies"
         guard let url = URL(string: urlString) else {
-            return nil
+            completion(nil)
+            return
         }
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                completion(nil)
                 return
             }
             
             do {
                 movies = try JSONDecoder().decode([Movie].self, from: data)
-                //print(movies)
+                completion(movies)
             } catch (let error) {
                 print(error)
             }
         }
         
         task.resume()
-        
-        return movies
     }
 }
